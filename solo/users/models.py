0e2a3fb87ffdf52bb4ae1dc661c9cdad476e0af0
Gerleff <gerleffx2@gmail.com>
2021-03-11 06:25:35 +0300

    log for debug

--- solo/users/models.py
+++ solo/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.CharField('Логин', max_length=220)
+    name = models.CharField('Логин', max_length=220, null=True)
     user_id = models.IntegerField("Телеграм айди")
     
     class Meta:
