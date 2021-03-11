e132606369c974b634bb10e96a84c0a6d9ad44b4
Gerleff <gerleffx2@gmail.com>
2021-03-11 05:29:46 +0300

    postgres

--- solo/tosub/models.py
+++ solo/tosub/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Tosub(models.Model):
     link = models.CharField('Ссылка', max_length=220)
     short = models.CharField("Сокращенное название", max_length=220)
-    ch_id = models.IntegerField("Айди канала")
+    ch_id = models.CharField("Айди канала", max_length=50)
