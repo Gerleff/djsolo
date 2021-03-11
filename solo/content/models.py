99aa57ec385c2f038b4657725954c3178d878350
Gerleff <gerleffx2@gmail.com>
2021-03-11 05:21:35 +0300

    postgres

--- solo/content/models.py
+++ solo/content/models.py
@@ -7,9 +7,9 @@ class Film(models.Model):
     names = models.CharField('Название', max_length=220)
     ugly_name = models.CharField('Название для поиска', max_length=220)
     links = models.URLField("Ссылка", max_length=220)
-    kp_rate = models.FloatField("КП рейтинг", null=True)
+    kp_rate = models.CharField("КП рейтинг", null=True, max_length=50)
     kp_links = models.URLField("КП ссылка", max_length=220, null=True)
-    imdb_rate = models.FloatField("ИМДБ рейтинг", null=True)
+    imdb_rate = models.CharField("ИМДБ рейтинг", null=True, max_length=50)
     imdb_links = models.URLField("ИМДБ ссылка", max_length=220, null=True)
     included = models.BooleanField("Включен в выдачу", default=True)
 
@@ -21,9 +21,9 @@ class Serial(models.Model):
     names = models.CharField('Название', max_length=220)
     ugly_name = models.CharField('Название для поиска', max_length=220)
     links = models.URLField("Ссылка", max_length=220)
-    kp_rate = models.FloatField("КП рейтинг", null=True)
+    kp_rate = models.CharField("КП рейтинг", null=True, max_length=50)
     kp_links = models.URLField("КП ссылка", max_length=220, null=True)
-    imdb_rate = models.FloatField("ИМДБ рейтинг", null=True)
+    imdb_rate = models.CharField("ИМДБ рейтинг", null=True, max_length=50)
     imdb_links = models.URLField("ИМДБ ссылка", max_length=220, null=True)
     included = models.BooleanField("Включен в выдачу", default=True)
 
