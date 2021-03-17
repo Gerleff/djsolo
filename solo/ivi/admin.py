dd3d6e7e2684a62844076ccc8a2faa0581387116
Gerleff <gerleffx2@gmail.com>
2021-03-17 16:52:37 +0300

    ivi added

--- solo/ivi/admin.py
+++ solo/ivi/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from solo.ivi.models import Film, Serial
+
+
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Serial)
+class SerialAdmin(admin.ModelAdmin):
+    pass
